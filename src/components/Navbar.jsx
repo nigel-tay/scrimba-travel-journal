91fcd79719f1eb44364c55a94c3b52c3100fa2ee
nigel-tay <ninenigel@gmail.com>
2022-11-25 15:33:00 +0800

    Complete styling for Navbar and individual TravelCard components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,8 @@ import { FaGlobeAmericas } from 'react-icons/fa';
 export default function Navbar() {
     return (
         <div className="navbar-container">
-            <FaGlobeAmericas />
-            <h2>my travel journal</h2>
+            <FaGlobeAmericas className="globe-icon" />
+            <h2 className="navbar-text">my travel journal</h2>
         </div>
     )
 }
--- src/components/TravelCard.jsx
+++ src/components/TravelCard.jsx
@@ -7,14 +7,25 @@ import keelung from "../assets/images/keelung.jpg";
 export default function TravelCard() {
     return (
         <div className="travelcard-container">
-            <img />
-            <div className="location-section">
-                <FaMapMarkerAlt />
-                <h3>COUNTRY TO CHANGE</h3> {/* COUNTRY */}
-                <p>GOOGLE MAPS LINK</p> {/* GOOGLE MAPS LINK */}
+            <div className="row-container">
+                <img className="card-image" src={batur} />
+                <div className="text-container">
+                    <div className="location-section">
+                        <FaMapMarkerAlt className="marker-icon" />
+                        <h3>COUNTRY TO CHANGE</h3> {/* COUNTRY */}
+                        <p className="google-link">
+                            <a href="#">View on Google Maps</a> {/* GOOGLE MAPS LINK */}
+                        </p> 
+                    </div>
+                    <h1 className="location-text">LOCATION</h1> {/* LOCATION */}
+                    <p className="date-range">DATE RANGE</p>
+                    <p className="description-text">
+                        It is a long established fact 
+                        that a reader will be distracted by the readable content of a page when looking at its
+                    </p> {/* DESCRIPTION */}
+                </div>
             </div>
-            <h1>LOCATION</h1> {/* LOCATION */}
-            <p>DESCRIPTION</p> {/* DESCRIPTION */}
+            <hr className="horizontal-rule"/> {/* has to be conditional to see if it is the last card or not */}
         </div>
     )
 }
